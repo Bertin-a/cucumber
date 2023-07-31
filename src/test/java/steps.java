b3e269920972b85ebe453b29c218ac39a80fa42f
Bertina Ayuure <bertina.ayuure@amalitech.com>
2023-07-31 16:10:52 +0000

    fixed issues with websocket

--- src/test/java/steps.java
+++ src/test/java/steps.java
@@ -5,26 +5,30 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class steps {
     private WebDriver driver;
 
     @Given("I am in the login page of the Para Bank Application")
-    public void i_am_in_the_login_page_of_the_para_bank_application() {
+    public void i_am_in_the_login_page_of_the_para_bank_application() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.get("https://parabank.parasoft.com/parabank/");
     }
     @When("I enter valid credentials")
     public void i_enter_valid_credentials() {
         driver.findElement(By.name("username")).sendKeys("test-ignore");
         driver.findElement(By.name("password")).sendKeys("password");
+        driver.findElement(By.cssSelector("input[class='button']")).click();
 
     }
     @Then("I should be taken to the overview page")
     public void i_should_be_taken_to_the_overview_page() throws InterruptedException {
         Thread.sleep(5000);
-        driver.findElement(By.cssSelector("[id='rightPanel']>h1")).isDisplayed();
-        driver.findElement(By.linkText("Log out")).click();
+        driver.findElement(By.className("title")).isDisplayed();
+        driver.findElement(By.linkText("Log Out")).click();
     }
 }
